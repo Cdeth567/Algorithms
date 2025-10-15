f7f96326ac44f53e18ef042028ccbb56cbc43024
Cdeth567 <125758823+Cdeth567@users.noreply.github.com>
2025-10-16 00:39:10 +0300

    Update BubbleSortAlgorithm.java

--- BubbleSortAlgorithm.java
+++ BubbleSortAlgorithm.java
@@ -3,7 +3,6 @@ Brief description of the problem:
 Given a nearly sorted sequence of integers, efficiently sort it.
 Implement a separate sorting function that either returns a new sorted array or sorts the input array in place.
 */
-
 import java.util.Scanner;
 
 public class BubbleSortAlgorithm {
